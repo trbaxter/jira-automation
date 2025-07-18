63b6b9669879abb5865e0b4a4c4796b9d3c906a7
Tyler Baxter <trb7074@gmail.com>
2025-07-17 20:45:13 -0500

    Add: api error handle function

--- src/automation_start.py
+++ src/automation_start.py
@@ -40,3 +40,19 @@ def generate_sprint_name(start_date, end_date) -> str:
                    f"({start_date.strftime("%m/%d")}-"
                    f"{end_date.strftime('%m/%d')}")
     return sprint_name
+
+# Log errors in workflow during calls to Jira API
+def handle_api_error(response, context) -> bool:
+    if response.status_code not in [200, 201, 204]:
+        logging.error(
+            f"\nError during {context}. Status Code: {response.status_code}"
+        )
+
+        if response.status_code == 504:
+            logging.error("Gateway timeout occurred.")
+
+        else:
+            logging.error(f"Response content: {response.text}")
+
+        return False
+    return True
