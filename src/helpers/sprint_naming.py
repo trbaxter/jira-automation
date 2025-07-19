ff1fd11d9e3d780c0ad70d6fa251fd4cbfb570ae
Tyler Baxter <trb7074@gmail.com>
2025-07-19 09:31:36 -0500

    Changing comment style to Google docstring

--- src/helpers/sprint_naming.py
+++ src/helpers/sprint_naming.py
@@ -1,14 +1,22 @@
-# Dynamically generate sprint name if needed
-def generate_sprint_name(start_date, end_date) -> str:
+from datetime import datetime
+
+def generate_sprint_name(start_date: datetime, end_date: datetime) -> str:
+    """
+    Dynamically generates sprint names.
+
+    Gets used if a new sprint isn't found in the backlog.
+    Uses the following naming convention:
+    <Some sprint name> <start date as YYMMDD> (start MM/DD - ending MM/DD)
+
+    Parameters:
+        start_date (datetime): The sprint's start date.
+        end_date (datetime): The sprint's end date.
+
+    Returns:
+        str: A formatted sprint name as a string.
+    """
     sprint_name = (
-      f"<Sprint Name> {start_date.strftime("%y%m%d")} "
-      f"({start_date.strftime("%m/%d")}-{end_date.strftime('%m/%d')})"
+        f"<Sprint Name> {start_date.strftime('%y%m%d')} "
+        f"({start_date.strftime('%m/%d')}-{end_date.strftime('%m/%d')})"
     )
     return sprint_name
-
-'''
-Used to dynamically generate sprint names if one isn't found in the backlog.
-Uses the following naming convention:
-
-<Some sprint name> <start date as YYMMDD> (start MM/DD - ending MM/DD)
-'''
