d5e8cd901d05f10d7c22f067dfef285278b1569a
Tyler Baxter <trb7074@gmail.com>
2025-07-19 16:22:07 -0500

    Added coverage for configure_logging

---
+++ test/unit/test_configure_logging.py
@@ -0,0 +1,19 @@
+import logging
+from _pytest.logging import LogCaptureFixture
+from src.logging_config.configure_logging import log_config
+
+
+def test_configure_logging_sets_level() -> None:
+    log_config(logging.DEBUG)
+    logger = logging.getLogger()
+    assert logger.getEffectiveLevel() == logging.DEBUG
+
+
+def test_configure_logging_sets_format(caplog: LogCaptureFixture) -> None:
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    with caplog.at_level(logging.INFO):
+        logger.info("Hello world")
+
+    assert "Hello world" in caplog.text
