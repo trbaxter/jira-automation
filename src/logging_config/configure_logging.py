80802924e057c097ee01b9faa609ef3133f30c90
Tyler Baxter <trb7074@gmail.com>
2025-07-19 16:21:46 -0500

    Added Google style docstring

--- src/logging_config/configure_logging.py
+++ src/logging_config/configure_logging.py
@@ -1,7 +1,22 @@
 import logging
 
-def configure_logging(level: int = logging.INFO) -> None:
+def log_config(level: int = logging.INFO) -> None:
+    """
+    Sets up the root logger using a message-only format.
+
+    Configures Python's root logger to use the specified logging level and
+    a minimal formatter that outputs only the message text, as the repository
+    workflow action that the logs will display in already contain timestamps.
+
+    Args:
+        level: Logging level to set. Can use 'logging.INFO', 'logging.DEBUG',
+               or 'logging.ERROR'. Defaults to 'logging.INFO'.
+
+    Returns:
+        None
+    """
     logging.basicConfig(
-        level = level,
-        format = "%(message)s"
+        level=level,
+        format="%(message)s",
+        force=True
     )
