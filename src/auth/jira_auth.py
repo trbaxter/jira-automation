4ff08d91948b0b6079ab0a086df879ab787e09d8
Tyler Baxter <trb7074@gmail.com>
2025-07-18 22:38:30 -0500

    Split up 2 functions to 3 for SSR

--- src/auth/jira_auth.py
+++ src/auth/jira_auth.py
@@ -3,18 +3,18 @@ import base64
 
 def get_jira_credentials() -> tuple[str, str]:
     email = os.getenv("JIRA_EMAIL")
-    api_token = os.getenv("JIRA_API_TOKEN")
-
-    if not email or not api_token:
+    token = os.getenv("JIRA_API_TOKEN")
+    if not email or not token:
         raise EnvironmentError("Email or API token not found.")
-    return email, api_token
+    return email, token
 
+def make_basic_auth_token(email: str, token: str) -> str:
+    credentials = f"{email}:{token}"
+    return base64.b64decode(credentials.encode()).decode("utf-8")
 
 def get_auth_header() -> dict[str, str]:
-    email, api_token = get_jira_credentials()
-    basic_auth = f"{email}:{api_token}"
-    encoded_token = base64.b64decode(basic_auth.encode()).decode("utf-8")
-
+    email, token = get_jira_credentials()
+    encoded_token = make_basic_auth_token(email, token)
     return {
         "Authorization": f"Basic {encoded_token}",
         "Content-Type": "application/json"
