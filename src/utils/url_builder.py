4a60381a2ca2f08cb3c294942edd047ca5012408
Tyler Baxter <trb7074@gmail.com>
2025-07-20 17:54:41 -0500

    added url builder function for sprints by state

---
+++ src/utils/url_builder.py
@@ -0,0 +1,6 @@
+def build_sprint_state_query_url(
+        base_url: str,
+        board_id: int,
+        state: str
+) -> str:
+    return f"{base_url}/rest/agile/1.0/board/{board_id}/sprint?state={state}"
