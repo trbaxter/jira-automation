9aae27a9f8c80c2f03924418323a6be9064614bc
Tyler Baxter <trb7074@gmail.com>
2025-07-22 16:51:00 -0500

    added test for sprint orchestration

--- tests/unit/test_sprint_orchestration.py
+++ tests/unit/test_sprint_orchestration.py
@@ -69,54 +69,55 @@ def test_use_future_backlog_sprint(
     mock_get_state.side_effect = fake_get_sprint_by_state
     mock_get_issues.return_value = [{"key": ISSUE1}]
 
-    automate_sprint(
-        MOCK_BOARD_NAME,
-        mock_session
-    )
+    automate_sprint(MOCK_BOARD_NAME, mock_session)
 
     mock_create.assert_not_called()
     mock_close.assert_called_once()
     mock_move_issues.assert_called_once()
     mock_start_sprint.assert_called_once_with(
-        new_sprint_id=100,
-        sprint_name="DART 250721 (07/21-08/04)",
-        start_date=ANY, # datetime.now()
-        end_date=ANY,   # datetime.now() + 2 weeks
-        session=mock_session,
-        base_url=MOCK_BASE_URL
+        100,
+        "DART 250721 (07/21-08/04)",
+        ANY, # datetime.now()
+        ANY, # datetime.now() + 2 weeks
+        mock_session,
+        MOCK_BASE_URL
     )
 
-# @patch("src.orchestration.sprint_lifecycle.get_board_config")
-# @patch("src.orchestration.sprint_lifecycle.get_sprint_by_state")
-# @patch("src.orchestration.sprint_lifecycle.create_sprint")
-# @patch("src.orchestration.sprint_lifecycle.get_incomplete_stories")
-# @patch("src.orchestration.sprint_lifecycle.close_sprint")
-# @patch("src.orchestration.sprint_lifecycle.move_issues_to_new_sprint")
-# @patch("src.orchestration.sprint_lifecycle.start_sprint")
-# @patch("src.orchestration.sprint_lifecycle.generate_sprint_name")
-# def test_create_sprint_when_non_dart_future(
-#         mock_generate_name,
-#         mock_start_sprint,
-#         mock_move_issues,
-#         mock_close,
-#         mock_create,
-#         mock_get_state,
-#         mock_get_config,
-#         mock_session,
-#         mock_config
-# ):
-#     # Mock non-DART future sprint
-#     mock_get_config.return_value = mock_config
-#     mock_get_state.side_effect = [
-#         {"id": 1, "name": "Team Planning"},  # future
-#         None  # active
-#     ]
-#     mock_create.return_value = {"id": 2}
-#     mock_generate_name.return_value = "DART 250721 (07/21-08/04)"
-#
-#     automate_sprint(board_name="TEST", session=mock_session)
-#
-#     mock_create.assert_called_once()
-#     mock_start_sprint.assert_called_once()
-#     mock_move_issues.assert_not_called()
-#     mock_close.assert_not_called()
+
+@patch(ORCH_CREATE_NAME)
+@patch(ORCH_START_SPRINT)
+@patch(ORCH_MOVE_ISSUES)
+@patch(ORCH_CLOSE_SPRINT)
+@patch(ORCH_GET_STORIES)
+@patch(ORCH_CREATE_SPRINT)
+@patch(ORCH_GET_SPRINT)
+def test_automate_sprint_create_new_sprint(
+        mock_get_sprint,
+        mock_create,
+        _mock_get_issues,
+        mock_close,
+        mock_move_issues,
+        mock_start_sprint,
+        mock_generate_name,
+        mock_session
+):
+    def fake_get_sprint(session, config, state):
+        _ = session, config # Unused, but required for function
+
+        if state == FUTURE:
+            return {"id": 1, "name": "Non-DART Sprint"}
+        if state == ACTIVE:
+            return None
+        return None
+
+    mock_get_sprint.side_effect = fake_get_sprint
+    mock_create.return_value = {"id": 2}
+    mock_generate_name.return_value = "DART 250721 (07/21-08/04)"
+
+    automate_sprint("TEST", mock_session)
+
+    mock_create.assert_called_once()
+    mock_start_sprint.assert_called_once()
+    mock_close.assert_not_called()
+    mock_move_issues.assert_not_called()
+
