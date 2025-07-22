596a1cb92df8b30d248fac72c22456a5600e09c4
Tyler Baxter <trb7074@gmail.com>
2025-07-22 10:52:43 -0500

    Adjusted parameter names to match elsewhere, used test constants

--- tests/unit/test_config_loader.py
+++ tests/unit/test_config_loader.py
@@ -4,13 +4,14 @@ import pytest
 
 from src.utils.config_loader import load_config
 from src.type_defs.boardconfig import BoardConfig
+from tests.constants.test_constants import TEST_YAML_PATH
 
 VALID_CONFIG_YAML = """
 boards:
     test:
-        id: 4
+        board_id: 4
         base_url: "https://test123.atlassian.net/"
-        name: "Some Fake Scrum Board"
+        board_name: "Some Fake Scrum Board"
 """
 
 MISSING_BOARDS_YAML = """
@@ -18,23 +19,20 @@ not_boards_attribute:
     some_other_thing: true
 """
 
-REAL_PATH = "board_config.yaml"
-TEST_PATH = "some_dummy_path.yaml"
-
 
 class TestLoadConfig:
     def test_loads_valid_config_with_mock(self) -> None:
         with (patch("pathlib.Path.open",
                     mock_open(read_data=VALID_CONFIG_YAML)),
               patch("pathlib.Path.exists", return_value=True)):
-            result = load_config(TEST_PATH)
+            result = load_config(TEST_YAML_PATH)
             assert isinstance(result, dict)
             assert "test" in result
 
             board: BoardConfig = result["test"]
-            assert board["id"] == 4
+            assert board["board_id"] == 4
             assert board["base_url"] == "https://test123.atlassian.net/"
-            assert board["name"] == "Some Fake Scrum Board"
+            assert board["board_name"] == "Some Fake Scrum Board"
 
     def test_raises_key_error_on_missing_boards(self) -> None:
         with (patch("pathlib.Path.open",
@@ -43,10 +41,12 @@ class TestLoadConfig:
             with pytest.raises(
                     KeyError,
                     match="Required section 'boards' missing"):
-                load_config(TEST_PATH)
+                load_config(TEST_YAML_PATH)
 
     def test_raises_file_not_found_on_missing_file(self) -> None:
         with patch("pathlib.Path.exists", return_value=False):
-            with pytest.raises(FileNotFoundError,
-                               match="board_config.yaml not found"):
-                load_config(TEST_PATH)
+            with pytest.raises(
+                    FileNotFoundError,
+                    match="board_config.yaml not found"
+            ):
+                load_config(TEST_YAML_PATH)
