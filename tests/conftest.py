74c4c0bc55b0363eb680fbc20976d65ed868a70a
Tyler Baxter <trb7074@gmail.com>
2025-07-22 11:41:55 -0500

    reformat code + optimize imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,14 @@
 import logging
+
 import pytest
-from tests.constants.patch_targets import (
-    BOARD_CONFIG
-)
+
+from tests.constants.patch_targets import (BOARD_CONFIG, JSPRINT_GET_CONFIG)
 from tests.constants.test_constants import (
     MOCK_BASE_URL,
-    MOCK_BOARD_NAME
+    MOCK_BOARD_NAME,
+    YAML_CONFIG_PATH
 )
+
 
 @pytest.fixture(autouse=True)
 def reset_root_handlers():
@@ -21,9 +23,9 @@ def patch_get_board_config(monkeypatch):
 
     def _mock_get_board_config(
             board_name: str,
-            path: str = "board_config.yaml"
+            path: str = YAML_CONFIG_PATH
     ) -> BoardConfig:
-        _ = board_name, path
+        _ = board_name, path  # Required in function, but not used
         return {
             "board_id": 1,
             "base_url": MOCK_BASE_URL,
@@ -31,4 +33,4 @@ def patch_get_board_config(monkeypatch):
         }
 
     monkeypatch.setattr(BOARD_CONFIG, _mock_get_board_config)
-    monkeypatch.setattr("src.services.jira_sprint.get_board_config", _mock_get_board_config)
+    monkeypatch.setattr(JSPRINT_GET_CONFIG, _mock_get_board_config)
