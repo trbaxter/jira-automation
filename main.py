47c066463bb44df0ef152a6d15c0d9848ce5a361
Tyler Baxter <trb7074@gmail.com>
2025-07-23 15:02:22 -0500

    modifying config for debug

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ from src.orchestration.sprint_orchestration import automate_sprint
 
 if __name__ == "__main__":
     try:
-        board_name = os.environ.get("test")
-        if not board_name:
+        board_key = os.environ.get("BOARD")
+        if not board_key:
             raise ValueError("YAML board environment variable is not set.")
 
-        config = get_board_config(board_name)
+        config = get_board_config(board_key)
         session = get_authenticated_session()
-
-        automate_sprint(board_name, session)
+        automate_sprint(board_key, session)
 
     except SystemExit as e:
         logging.error(f"Process terminated: {e}")
