64292705181ed198cd9312b7dcc422cf03029556
Tyler Baxter <trb7074@gmail.com>
2025-07-22 19:40:41 -0500

    consolidated tests into a class to avoid writing same @patch repeatedly

--- tests/unit/test_sprint_orchestration.py
+++ tests/unit/test_sprint_orchestration.py
@@ -20,179 +20,156 @@ from tests.constants.test_constants import (
     MOCK_BOARD_NAME
 )
 
-
-@pytest.fixture
-def mock_session():
-    return MagicMock()
-
-
-@patch(ORCH_START_SPRINT)
-@patch(ORCH_MOVE_ISSUES)
+# Note: @patch listing in alphabetical order
 @patch(ORCH_CLOSE_SPRINT)
-@patch(ORCH_GET_STORIES)
+@patch(ORCH_CREATE_NAME)
 @patch(ORCH_CREATE_SPRINT)
 @patch(ORCH_GET_SPRINT)
-@patch(ORCH_CREATE_NAME)
-def test_use_future_backlog_sprint(
-        _mock_generate_name,
-        mock_get_state,
-        mock_create,
-        mock_get_issues,
-        mock_close,
-        mock_move_issues,
-        mock_start_sprint,
-        mock_session,
-):
-    def fake_get_sprint_by_state(session, config, state):
-        if state == FUTURE:
+@patch(ORCH_GET_STORIES)
+@patch(ORCH_MOVE_ISSUES)
+@patch(ORCH_START_SPRINT)
+class TestAutomateSprint:
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.mock_session = MagicMock()
+
+    def test_use_future_backlog_sprint(
+            self,
+            mock_start_sprint,
+            mock_move_issues,
+            mock_get_stories,
+            mock_get_sprint,
+            mock_create_sprint,
+            _mock_create_name,
+            mock_close_sprint,
+    ):
+        def fake_get_sprint_by_state(session, config, state):
+            if state == FUTURE:
+                _ = session, config  # Unused, but required for function
+
+                # Future backlog sprint
+                return {
+                    "id": 100,
+                    "name": "DART 250721 (07/21-08/04)"
+                }
+
+            elif state == ACTIVE:
+                return {
+                    "id": 99,
+                    "name": "DART 240707",
+                    "startDate": "2024-07-07T10:00:00.000Z",
+                    "endDate": "2024-07-21T10:00:00.000Z",
+                    "session": self.mock_session,
+                    "base_url": MOCK_BASE_URL
+                }
+            else:
+                return None
+
+        mock_get_sprint.side_effect = fake_get_sprint_by_state
+        mock_get_stories.return_value = [{"key": ISSUE1}]
+
+        automate_sprint(MOCK_BOARD_NAME, self.mock_session)
+
+        mock_create_sprint.assert_not_called()
+        mock_close_sprint.assert_called_once()
+        mock_move_issues.assert_called_once()
+        mock_start_sprint.assert_called_once_with(
+            100,
+            "DART 250721 (07/21-08/04)",
+            ANY,  # datetime.now()
+            ANY,  # datetime.now() + 2 weeks
+            self.mock_session,
+            MOCK_BASE_URL
+        )
+
+    def test_automate_sprint_create_new_sprint(
+            self,
+            mock_start_sprint,
+            mock_move_issues,
+            _mock_get_stories,
+            mock_get_sprint,
+            mock_create_sprint,
+            mock_create_name,
+            mock_close_sprint,
+    ):
+        def fake_get_sprint(session, config, state):
             _ = session, config  # Unused, but required for function
 
-            # Future backlog sprint
-            return {
-                "id": 100,
-                "name": "DART 250721 (07/21-08/04)"
-            }
-
-        elif state == ACTIVE:
-            return {
-                "id": 99,
-                "name": "DART 240707",
-                "startDate": "2024-07-07T10:00:00.000Z",
-                "endDate": "2024-07-21T10:00:00.000Z",
-                "session": mock_session,
-                "base_url": MOCK_BASE_URL
-            }
-        else:
+            if state == FUTURE:
+                return {"id": 1, "name": "Non-DART Sprint"}
+            if state == ACTIVE:
+                return None
             return None
 
-    mock_get_state.side_effect = fake_get_sprint_by_state
-    mock_get_issues.return_value = [{"key": ISSUE1}]
+        mock_get_sprint.side_effect = fake_get_sprint
+        mock_create_sprint.return_value = {"id": 2}
+        mock_create_name.return_value = "DART 250721 (07/21-08/04)"
 
-    automate_sprint(MOCK_BOARD_NAME, mock_session)
+        automate_sprint("TEST", self.mock_session)
 
-    mock_create.assert_not_called()
-    mock_close.assert_called_once()
-    mock_move_issues.assert_called_once()
-    mock_start_sprint.assert_called_once_with(
-        100,
-        "DART 250721 (07/21-08/04)",
-        ANY,  # datetime.now()
-        ANY,  # datetime.now() + 2 weeks
-        mock_session,
-        MOCK_BASE_URL
-    )
+        mock_create_sprint.assert_called_once()
+        mock_start_sprint.assert_called_once()
+        mock_close_sprint.assert_not_called()
+        mock_move_issues.assert_not_called()
 
+    def test_log_if_no_future_sprint_creates_dart(
+            self,
+            mock_start_sprint,
+            _mock_move_issues,
+            _mock_get_stories,
+            mock_get_sprint,
+            mock_create_sprint,
+            mock_create_name,
+            _mock_close_sprint,
+            caplog
+    ):
+        def fake_get_sprint(session, config, state):
+            _ = session, config  # Unused, but required for function
 
-@patch(ORCH_CREATE_NAME)
-@patch(ORCH_START_SPRINT)
-@patch(ORCH_MOVE_ISSUES)
-@patch(ORCH_CLOSE_SPRINT)
-@patch(ORCH_GET_STORIES)
-@patch(ORCH_CREATE_SPRINT)
-@patch(ORCH_GET_SPRINT)
-def test_automate_sprint_create_new_sprint(
-        mock_get_sprint,
-        mock_create,
-        _mock_get_issues,
-        mock_close,
-        mock_move_issues,
-        mock_start_sprint,
-        mock_generate_name,
-        mock_session
-):
-    def fake_get_sprint(session, config, state):
-        _ = session, config  # Unused, but required for function
-
-        if state == FUTURE:
-            return {"id": 1, "name": "Non-DART Sprint"}
-        if state == ACTIVE:
+            if state == FUTURE:
+                return None
+            if state == ACTIVE:
+                return None
             return None
-        return None
 
-    mock_get_sprint.side_effect = fake_get_sprint
-    mock_create.return_value = {"id": 2}
-    mock_generate_name.return_value = "DART 250721 (07/21-08/04)"
+        mock_get_sprint.side_effect = fake_get_sprint
+        mock_create_name.return_value = "DART 250721 (07/21-08/04)"
+        mock_create_sprint.return_value = {"id": 123}
 
-    automate_sprint("TEST", mock_session)
+        with caplog.at_level("INFO"):
+            automate_sprint("TEST", self.mock_session)
 
-    mock_create.assert_called_once()
-    mock_start_sprint.assert_called_once()
-    mock_close.assert_not_called()
-    mock_move_issues.assert_not_called()
+        assert "No upcoming sprint found" in caplog.text
+        mock_create_sprint.assert_called_once()
+        mock_start_sprint.assert_called_once()
 
+    def test_log_if_create_sprint_returns_none(
+            self,
+            mock_start_sprint,
+            _mock_move_issues,
+            _mock_get_stories,
+            mock_get_sprint,
+            mock_create_sprint,
+            mock_create_name,
+            _mock_close_sprint,
+            caplog
+    ):
+        def fake_get_sprint(session, config, state):
+            _ = session, config  # Unused, but required for function
 
-@patch(ORCH_CREATE_NAME)
-@patch(ORCH_START_SPRINT)
-@patch(ORCH_MOVE_ISSUES)
-@patch(ORCH_CLOSE_SPRINT)
-@patch(ORCH_GET_STORIES)
-@patch(ORCH_CREATE_SPRINT)
-@patch(ORCH_GET_SPRINT)
-def test_log_if_no_future_sprint_creates_dart(
-        mock_get_sprint,
-        mock_create,
-        _mock_get_issues,
-        _mock_close,
-        _mock_move_issues,
-        mock_start_sprint,
-        mock_generate_name,
-        mock_session,
-        caplog
-):
-    def fake_get_sprint(session, config, state):
-        _ = session, config  # Unused, but required for function
-
-        if state == FUTURE:
+            if state == FUTURE:
+                return None
+            if state == ACTIVE:
+                return None
             return None
-        if state == ACTIVE:
-            return None
-        return None
 
-    mock_get_sprint.side_effect = fake_get_sprint
-    mock_generate_name.return_value = "DART 250721 (07/21-08/04)"
-    mock_create.return_value = {"id": 123}
+        mock_get_sprint.side_effect = fake_get_sprint
+        mock_create_name.return_value = "DART 250721 (07/21-08/04)"
+        mock_create_sprint.return_value = None
 
-    with caplog.at_level("INFO"):
-        automate_sprint("TEST", mock_session)
+        with caplog.at_level("ERROR"):
+            automate_sprint("TEST", self.mock_session)
 
-    assert "No upcoming sprint found" in caplog.text
-    mock_create.assert_called_once()
-    mock_start_sprint.assert_called_once()
-
-
-@patch(ORCH_CREATE_NAME)
-@patch(ORCH_START_SPRINT)
-@patch(ORCH_MOVE_ISSUES)
-@patch(ORCH_CLOSE_SPRINT)
-@patch(ORCH_GET_STORIES)
-@patch(ORCH_CREATE_SPRINT)
-@patch(ORCH_GET_SPRINT)
-def test_log_if_create_sprint_returns_none(
-        mock_get_sprint,
-        mock_create,
-        _mock_get_issues,
-        _mock_close,
-        _mock_move_issues,
-        mock_start_sprint,
-        mock_generate_name,
-        mock_session,
-        caplog
-):
-    def fake_get_sprint(session, config, state):
-        _ = session, config  # Unused, but required for function
-
-        if state == FUTURE:
-            return None
-        if state == ACTIVE:
-            return None
-        return None
-
-    mock_get_sprint.side_effect = fake_get_sprint
-    mock_generate_name.return_value = "DART 250721 (07/21-08/04)"
-    mock_create.return_value = None
-
-    with caplog.at_level("ERROR"):
-        automate_sprint("TEST", mock_session)
-
-    assert "Failed to create a new sprint." in caplog.text
-    mock_start_sprint.assert_not_called()
+        assert "Failed to create a new sprint." in caplog.text
+        mock_start_sprint.assert_not_called()
