b969cc1838f559ebc3ea0ef9769e1220c97445d0
Tyler Baxter <trb7074@gmail.com>
2025-07-22 20:07:47 -0500

    reformat code + optimize imports

--- tests/unit/test_datetime_format.py
+++ tests/unit/test_datetime_format.py
@@ -1,11 +1,13 @@
-import pytest
 from datetime import datetime
+
+import pytest
+
 from src.utils.datetime_format import format_jira_date
 from tests.constants.test_objects import JIRA_DATE_REGEX
 
 
 @pytest.mark.parametrize("dt", [
-    datetime(2024, 1 ,2 ,3 ,4 ,5),
+    datetime(2024, 1, 2, 3, 4, 5),
     datetime(2035, 12, 31, 23, 59, 59),
     datetime(9999, 1, 1, 0, 0, 0),
     datetime.now()
