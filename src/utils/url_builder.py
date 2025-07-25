d1db31e85fc902c11b41bde53edc9cbe5728aa4b
Tyler Baxter <trb7074@gmail.com>
2025-07-25 12:01:15 -0500

    preventing exceed of 80 characters per line

--- src/utils/url_builder.py
+++ src/utils/url_builder.py
@@ -5,4 +5,6 @@ def build_sprint_state_query_url(
         board_id: int,
         state: str
 ) -> str:
-    return f"{base_url}/rest/agile/1.0/board/{board_id}/sprint?state={state}"
+    return (
+        f"{base_url}/rest/agile/1.0/board/{board_id}/sprint?state={state}"
+    )
