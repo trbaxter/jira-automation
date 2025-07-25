c33a7ca4d041e53dc99627e5c25933f043fe0290
Tyler Baxter <trb7074@gmail.com>
2025-07-25 12:05:26 -0500

    cleanup obsolete parts

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 import logging
-import os
 
-from src.logging_config.configure_logging import log_config
 from src.auth.session import get_authenticated_session
-from src.helpers.config_accessor import get_board_config
+from src.logging_config.configure_logging import log_config
 from src.orchestration.sprint_orchestration import automate_sprint
+from src.utils.config_loader import load_config
 
 if __name__ == "__main__":
     log_config()
 
     try:
-        board_key = os.environ.get("BOARD")
-        if not board_key:
-            raise ValueError("YAML board environment variable is not set.")
-
-        config = get_board_config(board_key)
+        config = load_config()
         session = get_authenticated_session()
-        automate_sprint(board_key, session)
+        automate_sprint(session)
 
     except SystemExit as e:
         logging.error(f"Process terminated: {e}")
         raise
+
     except Exception as e:
         logging.exception("Unexpected error occurred.")
         raise
