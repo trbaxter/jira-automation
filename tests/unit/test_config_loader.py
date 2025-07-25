2077038fbdb31b808eb82511b347aeedeb3c87f2
Tyler Baxter <trb7074@gmail.com>
2025-07-25 17:05:00 -0500

    updated test conditions

--- tests/unit/test_config_loader.py
+++ tests/unit/test_config_loader.py
@@ -1,47 +1,40 @@
-from unittest.mock import mock_open, patch
+from unittest.mock import patch, mock_open
 
 import pytest
+from pydantic import HttpUrl
 
+from src.exceptions.config_not_found_error import ConfigNotFoundError
 from src.models.board_config import BoardConfig
 from src.utils.config_loader import load_config
-from tests.constants.test_objects import (
-    MISSING_BOARDS_YAML,
-    VALID_CONFIG_YAML
+
+_VALID_YAML = (
+    """
+    board_id: 123
+    base_url: "https://example.com/"
+    board_name: "Some Test Board"
+    """
 )
 
 
-class TestLoadConfig:
-    def test_loads_valid_config_with_mock(self) -> None:
-        with (
-            patch("pathlib.Path.open", mock_open(read_data=VALID_CONFIG_YAML)),
-            patch("pathlib.Path.exists", return_value=True)
-        ):
-            result = load_config()
-            assert isinstance(result, dict)
-            assert "test" in result
+# Valid file should return a valid BoardConfig object with valid attributes
+def test_load_config_success() -> None:
+    with patch("pathlib.Path.open", mock_open(read_data=_VALID_YAML)):
+        result = load_config()
+        assert isinstance(result, BoardConfig)
+        assert result.board_id == 123
+        assert result.base_url == HttpUrl("https://example.com/")
+        assert result.board_name == "Some Test Board"
 
-            board: BoardConfig = result["test"]
-            assert board["board_id"] == 4
-            assert board["base_url"] == "https://test123.atlassian.net/"
-            assert board["board_name"] == "Some Fake Scrum Board"
 
-    def test_raises_key_error_on_missing_boards(self) -> None:
-        with (
-            patch(
-                "pathlib.Path.open",
-                mock_open(read_data=MISSING_BOARDS_YAML)),
-            patch("pathlib.Path.exists", return_value=True)
-        ):
-            with pytest.raises(
-                    KeyError,
-                    match="Required section 'boards' missing"
-            ):
-                load_config()
+# ConfigNotFoundError should be raised if config file absent
+def test_load_config_missing_yaml() -> None:
+    with patch("pathlib.Path.open", side_effect=FileNotFoundError):
+        with pytest.raises(ConfigNotFoundError):
+            load_config()
 
-    def test_raises_file_not_found_on_missing_file(self) -> None:
-        with patch("pathlib.Path.exists", return_value=False):
-            with pytest.raises(
-                    FileNotFoundError,
-                    match="board_config.yaml not found"
-            ):
-                load_config()
+
+# Using a non-dictionary mapping yaml config should raise TypeError
+def test_load_config_malformed_yaml() -> None:
+    with patch("pathlib.Path.open", mock_open(read_data="abc123")):
+        with pytest.raises(TypeError):
+            load_config()
