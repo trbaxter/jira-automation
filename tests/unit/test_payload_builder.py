09449782de9b4d4c1d59b27a32938784d635d340
Tyler Baxter <trb7074@gmail.com>
2025-07-25 19:51:55 -0500

    test revamp using hypothesis

--- tests/unit/test_payload_builder.py
+++ tests/unit/test_payload_builder.py
@@ -1,24 +1,34 @@
-from utils.payload_builder import build_close_sprint_payload
-from tests.constants.test_constants import (
-    CLOSED,
-    MOCK_SPRINT_END,
-    MOCK_SPRINT_NAME,
-    MOCK_SPRINT_START
+import re
+from datetime import datetime, timedelta
+
+from hypothesis import given, assume
+from hypothesis.strategies import text, datetimes
+
+from src.utils.datetime_format import format_jira_date
+from src.utils.payload_builder import build_close_sprint_payload
+
+JIRA_DATE_REGEX = re.compile(r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.000\+\d{4}")
+
+
+@given(
+    name=text(min_size=1),  # Non-empty string
+    start_dt=datetimes(min_value=datetime(2025, 1, 1))
 )
+def test_build_close_sprint_payload_returns_expected_dict(
+        name: str,
+        start_dt: datetime
+) -> None:
+    assume(start_dt.year >= 2025)
+    end_dt = start_dt + timedelta(days=13)
 
+    start_str = format_jira_date(start_dt)
+    end_str = format_jira_date(end_dt)
 
-def test_build_close_sprint_payload_returns_expected_dict() -> None:
-    result = build_close_sprint_payload(
-        MOCK_SPRINT_NAME,
-        MOCK_SPRINT_START,
-        MOCK_SPRINT_END
-    )
+    result = build_close_sprint_payload(name, start_str, end_str)
 
-    expected = {
-        "state": CLOSED,
-        "name": MOCK_SPRINT_NAME,
-        "startDate": MOCK_SPRINT_START,
-        "endDate": MOCK_SPRINT_END
-    }
-
-    assert result == expected
+    assert result["state"] == "closed"
+    assert result["name"] == name
+    assert result["startDate"] == start_str
+    assert result["endDate"] == end_str
+    assert JIRA_DATE_REGEX.fullmatch(result["startDate"])
+    assert JIRA_DATE_REGEX.fullmatch(result["endDate"])
