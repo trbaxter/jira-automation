16f70d15cf77764763e7b046dec7434eb2ab8de3
Tyler Baxter <trb7074@gmail.com>
2025-07-26 18:11:38 -0500

    added a common strategies file for hypothesis testing

---
+++ tests/strategies/common.py
@@ -0,0 +1,12 @@
+from datetime import datetime, timedelta
+
+from hypothesis.strategies import text, datetimes
+
+clean_name = text(min_size=1).filter(
+    lambda s: s.strip() != "" and s == s.strip() and s.isprintable()
+)
+
+valid_date_range = datetimes(
+    min_value=datetime(2025, 1, 1),
+    max_value=datetime.now() + timedelta(days=365 * 100)
+)
