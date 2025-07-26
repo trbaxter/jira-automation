8e07aa4db571efefe2aab19d058449132289eb15
Tyler Baxter <trb7074@gmail.com>
2025-07-25 22:06:29 -0500

    fixed type warning + cleanup

--- tests/unit/test_url_builder.py
+++ tests/unit/test_url_builder.py
@@ -1,10 +1,12 @@
+from pydantic import HttpUrl
+
 from src.utils.url_builder import build_sprint_state_query_url
-from tests.constants.test_constants import ACTIVE, MOCK_BASE_URL
+
+_MOCK_BASE_URL = HttpUrl("https://mock.com/")
+_MOCK_FULL_URL = "https://mock.com/rest/agile/1.0/board/1/sprint?state=active"
 
 
 def test_build_sprint_state_query_url() -> None:
-    url = build_sprint_state_query_url(MOCK_BASE_URL, 1, ACTIVE)
+    url = build_sprint_state_query_url(_MOCK_BASE_URL, 1, "active")
 
-    assert url == (
-        f"{MOCK_BASE_URL}/rest/agile/1.0/board/1/sprint?state={ACTIVE}"
-    )
+    assert url == _MOCK_FULL_URL
