1d79e63b35a17d8ea252b06c80fbf45fd903e064
Tyler Baxter <trb7074@gmail.com>
2025-07-26 09:47:47 -0500

    adding pydantic typing for function arguments

--- src/utils/payload_builder.py
+++ src/utils/payload_builder.py
@@ -10,12 +10,12 @@ def build_close_sprint_payload(
         start_date: str,
         end_date: str
 ) -> CloseSprintPayload:
-    return {
-        "state": "closed",
-        "name": sprint_name,
-        "startDate": start_date,
-        "endDate": end_date,
-    }
+    return CloseSprintPayload(
+        state="closed",
+        name=sprint_name,
+        startDate=start_date,
+        endDate=end_date
+    )
 
 
 def build_start_sprint_payload(
@@ -23,9 +23,9 @@ def build_start_sprint_payload(
         start_date: datetime,
         end_date: datetime
 ) -> StartSprintPayload:
-    return {
-        "state": "active",
-        "name": sprint_name,
-        "startDate": format_jira_date(start_date),
-        "endDate": format_jira_date(end_date)
-    }
+    return StartSprintPayload(
+        state="active",
+        name=sprint_name,
+        startDate=format_jira_date(start_date),
+        endDate=format_jira_date(end_date)
+    )
