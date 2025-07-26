1b9227198c031e2a526bb79f1eef73ad5b771ce6
Tyler Baxter <trb7074@gmail.com>
2025-07-26 13:20:44 -0500

    added pydantic typing constraints

--- src/utils/url_builder.py
+++ src/utils/url_builder.py
@@ -1,8 +1,8 @@
-from pydantic import HttpUrl
+from pydantic import conint, constr, HttpUrl
 
 def build_sprint_state_query_url(
         base_url: HttpUrl,
-        board_id: int,
-        state: str
+        board_id: conint(gt=0),
+        state: constr(strip_whitespace=True, min_length=1)
 ) -> str:
     return f"{base_url}rest/agile/1.0/board/{board_id}/sprint?state={state}"
