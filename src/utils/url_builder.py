65f99084b2b238fae7c5e27744ed93d5f97a99af
Tyler Baxter <trb7074@gmail.com>
2025-07-25 22:03:31 -0500

    fixed type warning

--- src/utils/url_builder.py
+++ src/utils/url_builder.py
@@ -5,6 +5,4 @@ def build_sprint_state_query_url(
         board_id: int,
         state: str
 ) -> str:
-    return (
-        f"{base_url}rest/agile/1.0/board/{board_id}/sprint?state={state}"
-    )
+    return f"{base_url}rest/agile/1.0/board/{board_id}/sprint?state={state}"
