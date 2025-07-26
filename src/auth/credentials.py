acac9ba2ded4f19a4ae02d519a79d008a8d42fd1
Tyler Baxter <trb7074@gmail.com>
2025-07-26 11:18:52 -0500

    removing optional, utilizing pydantic

--- src/auth/credentials.py
+++ src/auth/credentials.py
@@ -1,13 +1,15 @@
 import base64
 import os
-from typing import NamedTuple, runtime_checkable, Protocol, Optional
+from typing import runtime_checkable, Protocol
+
+from pydantic import BaseModel, constr, ValidationError
 
 from src.exceptions.missing_secrets_error import MissingSecretsError
 
 
-class Credentials(NamedTuple):
-    email: str
-    token: str
+class Credentials(BaseModel):
+    email: constr(strip_whitespace=True, min_length=1)
+    token: constr(strip_whitespace=True, min_length=1)
 
 
 @runtime_checkable
@@ -18,7 +20,7 @@ class EnvReader(Protocol):
     that no class body is required to function.
     """
 
-    def __call__(self, key: str) -> Optional[str]: ...
+    def __call__(self, key: str) -> str: ...
 
 
 def get_jira_credentials(getenv: EnvReader = os.getenv) -> Credentials:
@@ -34,16 +36,15 @@ def get_jira_credentials(getenv: EnvReader = os.getenv) -> Credentials:
     email = getenv("JIRA_EMAIL")
     token = getenv("JIRA_API_TOKEN")
 
-    missing = []
-    if not email:
-        missing.append("JIRA_EMAIL")
-    if not token:
-        missing.append("JIRA_API_TOKEN")
-
-    if missing:
+    try:
+        return Credentials(email=email, token=token)
+    except ValidationError:
+        missing = []
+        if not email:
+            missing.append("JIRA_EMAIL")
+        if not token:
+            missing.append("JIRA_API_TOKEN")
         raise MissingSecretsError(missing)
-
-    return Credentials(email, token)
 
 
 def make_basic_auth_token(email: str, token: str) -> str:
@@ -75,8 +76,8 @@ def get_auth_header() -> dict[str, str]:
         A dictionary containing the Authorization and Content-Type headers to
         include in an HTTP request.
     """
-    email, token = get_jira_credentials()
-    encoded_token = make_basic_auth_token(email, token)
+    credentials = get_jira_credentials()
+    encoded_token = make_basic_auth_token(credentials.email, credentials.token)
     return {
         "Authorization": f"Basic {encoded_token}",
         "Content-Type": "application/json"
