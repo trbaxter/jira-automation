a7109acc60d54ef0bed86a031cba4b0bd9226f9a
Tyler Baxter <trb7074@gmail.com>
2025-07-26 18:46:35 -0500

    enhancing JiraIssue typing with pydantic

--- src/models/jira_issue.py
+++ src/models/jira_issue.py
@@ -1,8 +1,8 @@
-from typing import TypedDict
+from pydantic import BaseModel, constr
 
 
-class JiraIssue(TypedDict, total=True):
-    key: str
-    type: str
-    status: str
-    summary: str
+class JiraIssue(BaseModel, total=True):
+    key: constr(strip_whitespace=True, min_length=1)
+    type: constr(strip_whitespace=True, min_length=1)
+    status: constr(strip_whitespace=True, min_length=1)
+    summary: constr(strip_whitespace=True, min_length=1)
