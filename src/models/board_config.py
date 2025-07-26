c8a2efd3a07912e936f1c350660002277a423110
Tyler Baxter <trb7074@gmail.com>
2025-07-25 22:16:57 -0500

    enhancing typing with pydantic

--- src/models/board_config.py
+++ src/models/board_config.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, conint, constr, HttpUrl
 
 
 class BoardConfig(BaseModel):
-    board_id: int
+    board_id: conint(gt=0)
     base_url: HttpUrl
-    board_name: str
+    board_name: constr(strip_whitespace=True, min_length=1)
