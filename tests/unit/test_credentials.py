f4af624f4f77fbabc6e8934e10cc7668f5641b98
Tyler Baxter <trb7074@gmail.com>
2025-07-27 13:40:18 -0500

    fixing test for black-box behavior checks

--- tests/unit/test_credentials.py
+++ tests/unit/test_credentials.py
@@ -1,14 +1,18 @@
 import pytest
 from hypothesis import given, settings
+from pydantic import ValidationError
 
 from src.auth.credentials import get_jira_credentials
 from tests.strategies.common import clean_string
 
+EMAIL = "JIRA_EMAIL"
+TOKEN = "JIRA_API_TOKEN"
+
 
 @given(email=clean_string, token=clean_string)
-@settings(max_examples=100)
+@settings(max_examples=1000)
 def test_get_jira_credentials_success(email: str, token: str) -> None:
-    env_vars = {"JIRA_EMAIL": email, "JIRA_API_TOKEN": token}
+    env_vars = {EMAIL: email, TOKEN: token}
     getenv = lambda key: env_vars.get(key)
 
     creds = get_jira_credentials(getenv=getenv)
@@ -17,16 +21,24 @@ def test_get_jira_credentials_success(email: str, token: str) -> None:
     assert creds.token == token
 
 
-# @pytest.mark.parametrize("missing_key", ["JIRA_EMAIL", "JIRA_API_TOKEN"])
-# def test_get_jira_credentials_missing_key(missing_key: str) -> None:
-#     def getenv(key):
-#         env = {
-#             "JIRA_EMAIL": "test@example.com" if key != "JIRA_EMAIL" else None,
-#             "JIRA_API_TOKEN": "abcd1234" if key != "JIRA_API_TOKEN" else None
-#         }
-#         return env.get(key)
-#
-#     with pytest.raises(MissingSecretsError) as e:
-#         get_jira_credentials(getenv=getenv)
-#
-#     assert missing_key in str(e.value)
+@given(email=clean_string, token=clean_string)
+@settings(max_examples=1000)
+@pytest.mark.parametrize("missing_key", [EMAIL, TOKEN])
+def test_get_jira_credentials_missing_key(
+        email: str,
+        missing_key: str,
+        token: str
+) -> None:
+    def getenv(key):
+        env = {
+            EMAIL: email if key != EMAIL else None,
+            TOKEN: token if key != TOKEN else None
+        }
+        return env.get(key)
+
+    env_key_to_field = {EMAIL: "email", TOKEN: "token"}
+
+    with pytest.raises(ValidationError) as e:
+        get_jira_credentials(getenv=getenv)
+
+    assert env_key_to_field[missing_key] in str(e.value)
