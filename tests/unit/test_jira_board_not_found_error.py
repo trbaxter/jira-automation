72c025af247defab343de798f7a8e2a78ccbc33a
Tyler Baxter <trb7074@gmail.com>
2025-07-27 18:24:09 -0500

    added test file for JiraBoardNotFoundError

--- tests/unit/test_jira_board_not_found_error.py
+++ tests/unit/test_jira_board_not_found_error.py
@@ -12,9 +12,7 @@ def test_jira_board_not_found_raises_with_message(board_name: str) -> None:
     with pytest.raises(JiraBoardNotFoundError) as error:
         raise JiraBoardNotFoundError(board_name)
 
-    error_type = error.value.__class__.__name__
-
     assert isinstance(error.value, JiraBoardNotFoundError)
     assert isinstance(error.value, Exception)
+
     assert error.value.args[0] == expected_msg
-    assert repr(error.value) == f"{error_type}('{expected_msg}')"
