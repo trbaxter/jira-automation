67f71555263d09848a38027cfc335c11835d8c3a
Tyler Baxter <trb7074@gmail.com>
2025-07-27 13:18:06 -0500

    reformat code + optimize imports

--- src/models/credentials.py
+++ src/models/credentials.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, constr
 
+
 class Credentials(BaseModel):
     email: constr(strip_whitespace=True, min_length=1)
     token: constr(strip_whitespace=True, min_length=1)
