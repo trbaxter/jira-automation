3855b0d8e611960086350b71d3e951e494d36a70
Tyler Baxter <trb7074@gmail.com>
2025-07-27 16:46:01 -0500

    removing @settings, default already configured

--- tests/unit/test_datetime_format.py
+++ tests/unit/test_datetime_format.py
@@ -1,7 +1,7 @@
 import re
 from datetime import datetime
 
-from hypothesis import given, settings
+from hypothesis import given
 from hypothesis.strategies import datetimes
 
 from src.utils.datetime_format import format_jira_date
@@ -12,7 +12,6 @@ JIRA_DATE_REGEX = re.compile(
 
 
 @given(dt=datetimes(min_value=datetime(2025, 1, 1)))
-@settings(max_examples=5000)
 def test_format_jira_date_success(dt: datetime) -> None:
     result = format_jira_date(dt)
     assert JIRA_DATE_REGEX.fullmatch(result)
