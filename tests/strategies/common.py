6a845fc398e8c1ca2fa4c45af97435d2d4dcce0f
Tyler Baxter <trb7074@gmail.com>
2025-07-28 10:14:59 -0500

    adjusted strategy constants to functions

--- tests/strategies/common.py
+++ tests/strategies/common.py
@@ -2,13 +2,17 @@ from datetime import datetime, timedelta
 
 from hypothesis.strategies import text, datetimes
 
-_NOW = datetime.now()
-_HUNDRED_YEARS = datetime.now() + timedelta(days=365 * 100)
 
-valid_date_range = datetimes(min_value=_NOW, max_value=_HUNDRED_YEARS)
+def valid_datetime_range():
+    now = datetime.now()
+    hundred_years_later = datetime.now() + timedelta(days=365 * 100)
 
-clean_string = (
-    text(min_size=1)
-    .map(str.strip)
-    .filter(lambda string: string != "" and string.isprintable())
-)
+    return datetimes(min_value=now, max_value=hundred_years_later)
+
+
+def cleaned_string():
+    min_str = text(min_size=1)
+
+    return min_str.map(str.strip).filter(
+        lambda string: string != "" and string.isprintable()
+    )
