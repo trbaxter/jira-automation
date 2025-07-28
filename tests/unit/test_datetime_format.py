537128dc3f061c699f7d3a760a7b35dd75df5c7d
Tyler Baxter <trb7074@gmail.com>
2025-07-28 10:18:34 -0500

    updated test to use strategy function

--- tests/unit/test_datetime_format.py
+++ tests/unit/test_datetime_format.py
@@ -4,14 +4,14 @@ from datetime import datetime
 from hypothesis import given
 
 from src.utils.datetime_format import format_jira_date
-from tests.strategies.common import valid_date_range
+from strategies.common import valid_datetime_range
 
 JIRA_DATE_REGEX = re.compile(
     r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.000\+\d{4}$"
 )
 
 
-@given(dt=valid_date_range)
+@given(dt=valid_datetime_range())
 def test_format_jira_date_success(dt: datetime) -> None:
     result = format_jira_date(dt)
     assert JIRA_DATE_REGEX.fullmatch(result)
