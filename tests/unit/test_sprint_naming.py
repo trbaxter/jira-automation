176995dd035004e6e9c88515d651b49f9cf15d30
Tyler Baxter <trb7074@gmail.com>
2025-07-28 11:37:34 -0500

    test cleanup, no longer concerned with time zone

--- tests/unit/test_sprint_naming.py
+++ tests/unit/test_sprint_naming.py
@@ -2,29 +2,24 @@ import re
 from datetime import datetime, timedelta
 
 from hypothesis import given
-from hypothesis.strategies import datetimes
 
+from strategies.common import valid_datetime_range
 from utils.sprint_naming import generate_sprint_name
 
 
-@given(
-    start_date=datetimes(
-        min_value=datetime(2025, 1, 1),
-        max_value=datetime.now() + timedelta(days=365 * 100),
-    )
-)
+@given(valid_datetime_range())
 def test_sprint_name_generation(start_date: datetime) -> None:
     end_date = start_date + timedelta(days=14)
     result = generate_sprint_name(start_date, end_date)
 
-    prefix = start_date.strftime("%y%m%d")
-    start_fmt = start_date.strftime("%m/%d")
-    inclusive_end_fmt = (end_date - timedelta(days=1)).strftime("%m/%d")
-    suffix = f"({start_fmt}-{inclusive_end_fmt})"
+    prefix = start_date.strftime(format="%y%m%d")
+    start_fmt = start_date.strftime(format="%m/%d")
+    end_fmt = end_date.strftime(format="%m/%d")
+    suffix = f"({start_fmt}-{end_fmt})"
 
     assert result.startswith("DART ")
     assert prefix in result
     assert suffix in result
 
-    pattern = rf"^DART {prefix} \({start_fmt}-{inclusive_end_fmt}\)$"
-    assert re.fullmatch(pattern, result)
+    pattern = rf"^DART {prefix} \({start_fmt}-{end_fmt}\)$"
+    assert re.fullmatch(pattern=pattern, string=result)
