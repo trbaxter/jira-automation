b46e4c70088fb2920677e209a56a1a6977b6a2b9
Tyler Baxter <trb7074@gmail.com>
2025-07-28 08:43:07 -0500

    improving typing constraints for dates

--- src/models/sprint_payload.py
+++ src/models/sprint_payload.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel, conint, constr
 
+VALID_DATETIME_FORM = r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.000\+\d{4}$"
+
 
 class SprintPayload(BaseModel):
     name: constr(strip_whitespace=True, min_length=1)
-    startDate: str
-    endDate: str
+    startDate: constr(pattern=VALID_DATETIME_FORM)
+    endDate: constr(pattern=VALID_DATETIME_FORM)
     originBoardId: conint(gt=0)
