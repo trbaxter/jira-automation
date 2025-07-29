ab175bb5180f636921f95320aa4307d456b23bb2
Tyler Baxter <trb7074@gmail.com>
2025-07-28 21:19:49 -0500

    updating to use common fieldtypes

--- src/models/jira_issue.py
+++ src/models/jira_issue.py
@@ -1,8 +1,10 @@
-from pydantic import BaseModel, constr
+from pydantic import BaseModel
+
+from src.fieldtypes.common import SAFE_STR
 
 
 class JiraIssue(BaseModel):
-    key: constr(strip_whitespace=True, min_length=1)
-    type: constr(strip_whitespace=True, min_length=1)
-    status: constr(strip_whitespace=True, min_length=1)
-    summary: constr(strip_whitespace=True, min_length=1)
+    key: SAFE_STR
+    type: SAFE_STR
+    status: SAFE_STR
+    summary: SAFE_STR
