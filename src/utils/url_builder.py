cab1ed88cb1322214a4b673d34c4733f1cb3b402
Tyler Baxter <trb7074@gmail.com>
2025-07-29 12:34:27 -0500

    updating to use common fieldtypes

--- src/utils/url_builder.py
+++ src/utils/url_builder.py
@@ -5,5 +5,5 @@ from src.fieldtypes.common import INT_GT_0, SAFE_STR
 
 def build_sprint_state_query_url(
     base_url: HttpUrl, board_id: INT_GT_0, state: SAFE_STR
-) -> str:
+) -> SAFE_STR:
     return f"{base_url}rest/agile/1.0/board/{board_id}/sprint?state={state}"
