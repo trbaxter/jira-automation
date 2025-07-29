1490c66f2bbbe8502119c4a1714a3ebe57410036
Tyler Baxter <trb7074@gmail.com>
2025-07-29 12:25:22 -0500

    updating file to use common fieldtypes

--- src/utils/payload_builder.py
+++ src/utils/payload_builder.py
@@ -1,31 +1,25 @@
 from datetime import datetime
 
+from src.fieldtypes.common import SAFE_STR
 from src.models.sprint_close_payload import CloseSprintPayload
 from src.models.sprint_start_payload import StartSprintPayload
 from src.utils.datetime_format import format_jira_date
 
 
 def build_close_sprint_payload(
-        sprint_name: str,
-        start_date: str,
-        end_date: str
+    sprint_name: SAFE_STR, start_date: SAFE_STR, end_date: SAFE_STR
 ) -> CloseSprintPayload:
     return CloseSprintPayload(
-        state="closed",
-        name=sprint_name,
-        startDate=start_date,
-        endDate=end_date
+        state="closed", name=sprint_name, startDate=start_date, endDate=end_date
     )
 
 
 def build_start_sprint_payload(
-        sprint_name: str,
-        start_date: datetime,
-        end_date: datetime
+    sprint_name: SAFE_STR, start_date: datetime, end_date: datetime
 ) -> StartSprintPayload:
     return StartSprintPayload(
         state="active",
         name=sprint_name,
         startDate=format_jira_date(start_date),
-        endDate=format_jira_date(end_date)
+        endDate=format_jira_date(end_date),
     )
