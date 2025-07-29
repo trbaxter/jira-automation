f3fb9e4af240d68ae6fec73089c9b91cdcd51ad1
Tyler Baxter <trb7074@gmail.com>
2025-07-29 12:28:01 -0500

    added keyword arguments to strftime

--- src/utils/sprint_naming.py
+++ src/utils/sprint_naming.py
@@ -17,7 +17,8 @@ def generate_sprint_name(start_date: datetime, end_date: datetime) -> str:
         str: A formatted sprint name as a string.
     """
     sprint_name = (
-        f"DART {start_date.strftime('%y%m%d')} "
-        f"({start_date.strftime('%m/%d')}-{end_date.strftime('%m/%d')})"
+        f"DART {start_date.strftime(format='%y%m%d')} "
+        f"({start_date.strftime(format='%m/%d')}-"
+        f"{end_date.strftime(format='%m/%d')})"
     )
     return sprint_name
