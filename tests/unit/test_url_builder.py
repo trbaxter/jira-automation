ec29d1a9174b7c8c9aeaf38ed2f084939f546306
Tyler Baxter <trb7074@gmail.com>
2025-07-29 20:41:21 -0500

    mock URL variables to simple variable

--- tests/unit/test_url_builder.py
+++ tests/unit/test_url_builder.py
@@ -2,15 +2,15 @@ from pydantic import HttpUrl
 
 from src.utils.url_builder import build_sprint_state_query_url
 
-_MOCK_BASE_URL = HttpUrl("https://mock.com/")
-_MOCK_FULL_URL = "https://mock.com/rest/agile/1.0/board/1/sprint?state=active"
+MOCK_BASE_URL = HttpUrl("https://mock.com/")
+MOCK_FULL_URL = "https://mock.com/rest/agile/1.0/board/1/sprint?state=active"
 
 
 def test_build_sprint_state_query_url() -> None:
     url = build_sprint_state_query_url(
-        base_url=_MOCK_BASE_URL,
+        base_url=MOCK_BASE_URL,
         board_id=1,
         state="active"
     )
 
-    assert url == _MOCK_FULL_URL
+    assert url == MOCK_FULL_URL
