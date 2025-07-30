777ef74d3716b3403292b7f9edfd05030df433de
Tyler Baxter <trb7074@gmail.com>
2025-07-30 16:02:32 -0500

    removing header creation logic for HTTPBasicAuth use

--- src/auth/credentials.py
+++ src/auth/credentials.py
@@ -1,7 +1,5 @@
-import base64
 import os
 
-from src.constants.field_types import SAFE_STR
 from src.models.credentials import Credentials
 from src.models.env_reader import EnvReader
 
@@ -19,44 +17,3 @@ def get_jira_credentials(getenv: EnvReader = os.getenv) -> Credentials:
     email = getenv("JIRA_EMAIL")
     token = getenv("JIRA_API_TOKEN")
     return Credentials(email=email, token=token)
-
-
-def make_basic_auth_token(email: SAFE_STR, token: SAFE_STR) -> str:
-    """
-    Creates an encoded token.
-
-    Uses values of the email and API token repository secrets to generate a
-    base-64 encoded token required for authentication by the Jira API.
-
-    Args:
-        email: Email address of Jira admin capable of making sprint changes.
-        token: API token assigned to the above email address.
-
-    Returns:
-        String containing the base-64 encoded token.
-    """
-    credentials = f"{email}:{token}"
-    return base64.b64encode(credentials.encode()).decode("utf-8")
-
-
-def get_auth_header() -> dict[str, str]:
-    """
-    Constructs the HTTP authorization header for Jira API requests.
-
-    Retrieves the credentials, encodes them using base64, and formats the
-    header as required for Basic Authentication.
-
-    Returns:
-        A dictionary containing the Authorization and Content-Type headers to
-        include in an HTTP request.
-    """
-    credentials = get_jira_credentials()
-    email = credentials.email
-    token = credentials.token
-
-    encoded_token = make_basic_auth_token(email, token)
-
-    return {
-        "Authorization": f"Basic {encoded_token}",
-        "Content-Type": "application/json",
-    }
--- src/auth/session.py
+++ src/auth/session.py
@@ -1,7 +1,8 @@
 import certifi
 import requests
+from requests.auth import HTTPBasicAuth
 
-from src.auth.credentials import get_auth_header
+from src.auth.credentials import get_jira_credentials
 
 
 def get_authenticated_session() -> requests.Session:
@@ -14,7 +15,9 @@ def get_authenticated_session() -> requests.Session:
     Returns:
         A configured session object with authentication headers.
     """
+    creds = get_jira_credentials()
     session = requests.Session()
+    session.auth = HTTPBasicAuth(creds.email, creds.token)
     session.verify = certifi.where()
-    session.headers.update(get_auth_header())
+    session.headers.update({"Content-Type": "application/json"})
     return session
