82334c1157bc5876bb7e38243e6d796ec969b506
Tyler Baxter <trb7074@gmail.com>
2025-08-06 18:53:06 -0500

    resolved typing error

--- src/logs/configure_logging.py
+++ src/logs/configure_logging.py
@@ -1,7 +1,5 @@
 import logging
 
-from src.constants.shared import INT_GEQ_0
 
-
-def log_config(level: INT_GEQ_0 = logging.INFO, force: bool = True) -> None:
+def log_config(level: int = logging.INFO, force: bool = True) -> None:
     logging.basicConfig(level=level, format='%(message)s', force=force)
