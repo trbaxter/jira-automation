5d40b4f91e311a76debf6b5338c4f392625dbae2
Tyler Baxter <trb7074@gmail.com>
2025-08-08 11:21:55 -0500

    updating type to pydanticv2 custom type

--- src/logs/configure_logging.py
+++ src/logs/configure_logging.py
@@ -1,5 +1,7 @@
 import logging
 
+from src.customtypes.shared import PositiveInt
 
-def log_config(level: int = logging.INFO, force: bool = True) -> None:
+
+def log_config(level: PositiveInt = logging.INFO, force: bool = True) -> None:
     logging.basicConfig(level=level, format='%(message)s', force=force)
