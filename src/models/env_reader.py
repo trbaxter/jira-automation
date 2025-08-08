7266350177c4c60a9415c5d4a5bf7c41eb1a6b82
Tyler Baxter <trb7074@gmail.com>
2025-08-08 15:04:03 -0500

    adding in default argument to EnvReader for consistency & adjusting return type

--- src/models/env_reader.py
+++ src/models/env_reader.py
@@ -1,6 +1,13 @@
 from typing import runtime_checkable, Protocol
 
+from src.customtypes.shared import NonEmptyStr
+
 
 @runtime_checkable
 class EnvReader(Protocol):
-    def __call__(self, key: str) -> str | None: ...
+    def __call__(
+            self,
+            key: str,
+            default: str | None = None
+    ) -> NonEmptyStr | None:
+        ...
