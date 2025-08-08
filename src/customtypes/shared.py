18b40adced8e2ff84ad4a76a6e7ddb3fa91a8662
Tyler Baxter <trb7074@gmail.com>
2025-08-08 14:35:17 -0500

    nonNegInt and PosInt already exist in pydantic v2, classes not needed

--- src/customtypes/shared.py
+++ src/customtypes/shared.py
@@ -6,30 +6,10 @@ from pydantic_core import core_schema
 JIRA_DATETIME_REGEX = r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.000\+\d{4}$'
 
 
-class PositiveInt(int):
+class NonEmptyStr(str):
     @classmethod
     def __get_pydantic_core_schema__(
-            cls: Type['PositiveInt'],
-            _source: Any,
-            _handler: GetCoreSchemaHandler
-    ) -> core_schema.CoreSchema:
-        return core_schema.int_schema(gt=0)
-
-
-class NonNegativeInt(int):
-    @classmethod
-    def __get_pydantic_core_schema__(
-            cls: Type['NonNegativeInt'],
-            _source: Any,
-            _handler: GetCoreSchemaHandler
-    ) -> core_schema.CoreSchema:
-        return core_schema.int_schema(ge=0)
-
-
-class SafeStr(str):
-    @classmethod
-    def __get_pydantic_core_schema__(
-            cls: Type['SafeStr'],
+            cls: Type['NonEmptyStr'],
             _source: Any,
             _handler: GetCoreSchemaHandler
     ) -> core_schema.CoreSchema:
